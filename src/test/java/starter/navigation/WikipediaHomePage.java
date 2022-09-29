8a3c36f742a1f9a80948eedd66ec061b6015a42b
hoang duy <hoangduy1771@gmail.com>
2022-09-29 21:35:50 +0700

    Initial commit

---
+++ src/test/java/starter/navigation/WikipediaHomePage.java
@@ -0,0 +1,11 @@
+package starter.navigation;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://wikipedia.org")
+public class WikipediaHomePage extends PageObject {
+    public static void main(String[] args) {
+
+    }
+}
